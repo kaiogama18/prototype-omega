79bdcd353f57c910e61d35d8402ecb4de2574251
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2025-03-25 20:51:47 -0400

    [Add] - Interface
    Create Interact Interface

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public CharacterController controller;
     public Animator animations;
+    public float rotationPower = 1f;
 
     void Start()
     {
@@ -17,15 +18,11 @@ public class PlayerController : MonoBehaviour
     {
         float side = Input.GetAxis("Horizontal");
         float forward = Input.GetAxis("Vertical");
-
+        
         Vector3 walk = transform.forward * forward + transform.right * side;
 
         controller.Move(speed * Time.deltaTime * walk);
 
-        if( side !=0 )
-        {
-            this.transform.Rotate(0f, side, 0f, Space.World);
-        }
 
         if( forward != 0 || side !=0 )
         {
@@ -35,5 +32,30 @@ public class PlayerController : MonoBehaviour
         {
             animations.SetBool("IsRunning", false);
         }
+
+
+        #region Player Interact
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float interctRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interctRange);
+            foreach (Collider collider in colliderArray)
+            {
+                if(collider.CompareTag("Player"))
+                {
+                    continue;
+                }
+
+                Debug.Log("Collider está na posição do objeto: -> " + collider);
+                
+                //if(collider != null && collider.bounds.Contains(transform.position))
+                //{
+                //  Debug.Log("Collider está na posição do objeto: -> " + collider);
+                //}
+            }
+        }
+        #endregion
+
     }
 }
