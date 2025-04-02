c7e3c469a875689a798836e513f99228c1a9a609
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2025-04-01 20:21:16 -0400

    [add] - ajuste layout

---
+++ Assets/Inventory/Storage/Scripts/StorageManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StorageManager : MonoBehaviour, IInteractable
+{
+    [SerializeField] private UIDocument storageUI;
+    private Button btnClose;
+    
+    public void Interact()
+    {
+        if(storageUI == null)
+        {
+            storageUI = GetComponent<UIDocument>();
+        }
+        
+        storageUI.transform.gameObject.SetActive(true);
+
+        btnClose = storageUI.rootVisualElement.Q("BtnClose") as Button;
+        btnClose.RegisterCallback<ClickEvent>(OnClose);
+    }
+    private void OnDisable()
+    {
+        btnClose.UnregisterCallback<ClickEvent>(OnClose);
+    }
+
+    public void OnClose(ClickEvent evt)
+    {
+        storageUI.transform.gameObject.SetActive(false);
+        OnDisable();
+    }
+
+}
